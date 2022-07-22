127aa568d36eeae0fe507317f06d13b97d1b1eb0
Denis Maina <Denis.maina@student.moringaschool.com>
2022-07-22 12:05:14 +0300

    initial commit

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  console.log(directors)
+
+  // const directorMovies =director.movies.map((movie)=>{
+  //   return <li key={movie}>
+  //     {movie}
+  //   </li>
+  // })
+  const eachDirector= directors.map((director)=>{
+    return <div key={director.name}>
+      {director.name}
+      <ul>{director.movies.map((movie)=>{
+        return <li key={movie}>{movie}</li>
+      })}</ul>
+    </div>
+  });
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {eachDirector}
+  </div>
+  );
 }
 
 export default Directors;
